18f2f1301dee018b8c93b89080984d90f6360aac
ZEUS\m4rt00 <m4rt00@gmail.com>
2018-11-14 08:30:08 +0200

    initial commit

---
+++ src/MinimalAspNetCoreApi/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinimalAspNetCoreApi.Controllers
+{
+    [ApiController]
+    [Route("")]
+    [Route("ping")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("pong");
+        }
+    }
+}
